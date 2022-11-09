81e49975071887ca2b3be25b0fe0d441a6ec3ccd
KristinSeipStubbe <kristin.seip.stubbe@gmail.com>
2022-11-09 13:19:07 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Tony!");
+        System.out.println("Thank you for some great days learning Git and GitHub");
+
+
+    }
+}
---
+++ src/NewClass.java
@@ -0,0 +1,9 @@
+public class NewClass {
+    int age;
+    String name;
+
+    public NewClass(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
